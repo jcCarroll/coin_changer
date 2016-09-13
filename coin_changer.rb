33ba280cd2e912c7f973acdcc447a4c0148d0aaa
jacob <2staffi@gmail.com>
2016-09-13 16:08:05 -0400

    moved comment from test to function

--- coin_changer.rb
+++ coin_changer.rb
@@ -1,3 +1,9 @@
+# Coin changer kata
+
+# Write a function that takes a number of cents, and that returns a hash containing the least number of coins needed to generate the value of cents.
+
+# eg. if you have 11 cents you should return {:dime => 1, :penny => 1}; if you have 31 cents you should return {:quarter => 1, :nickle => 1, :penny => 1}.
+
 def coin_changer(cents)
 	{}
 end
