ea074a8c01eef185a3ec3d5536584067ff3e6bfd
jacob <2staffi@gmail.com>
2016-09-13 16:11:09 -0400

    if cents == 1

--- coin_changer.rb
+++ coin_changer.rb
@@ -5,5 +5,9 @@
 # eg. if you have 11 cents you should return {:dime => 1, :penny => 1}; if you have 31 cents you should return {:quarter => 1, :nickle => 1, :penny => 1}.
 
 def coin_changer(cents)
-	{}
+	coins = {}
+	if cents == 1
+		coins[:penny] = 1
+	end
+	coins
 end
