052fe0af9e585e8c7fb210ba733fdabca3dbc88b
jacob <2staffi@gmail.com>
2016-09-13 16:06:24 -0400

    1 cent equals 1 penny

--- coin_changer_test.rb
+++ coin_changer_test.rb
@@ -11,4 +11,9 @@ class TestCoinChanger <Minitest::Test
 	def test_0_cents_returns_empty_hash
 		assert_equal({}, coin_changer(0))
 	end
+
+	def test_1_cent_equals_1_penny
+		cents = 1
+		assert_equal({:penny => 1}, coin_changer(cents))
+	end
 end
