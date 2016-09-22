70f3f2c7124313c9952dd5264c5bd1cb08de041a
jacob <2staffi@gmail.com>
2016-09-22 19:33:55 -0400

    help function and erb

--- app.rb
+++ app.rb
@@ -3,3 +3,13 @@ require 'sinatra'
 get '/' do 
 	erb :get_name
 end
+
+post '/user_name' do
+	name = params[:user_name]
+	redirect '/help?user_name=' + name
+end
+
+get '/help' do 
+	name = params[:user_name].capitalize
+	erb :get_help, :locals => {:name => name}
+end
