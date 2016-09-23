fc78454fbf2d2fb3f6635b48bfac34cab47d4aad
jacob <2staffi@gmail.com>
2016-09-22 20:39:05 -0400

    help and change

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ get '/' do
 	erb :get_name
 end
 
-post '/user_name' do
+post '/name' do
 	name = params[:user_name]
 	redirect '/help?user_name=' + name
 end
@@ -13,3 +13,23 @@ get '/help' do
 	name = params[:user_name].capitalize
 	erb :get_help, :locals => {:name => name}
 end
+
+post '/help' do
+	name = params[:user_name]
+	if value = "Yes"
+		redirect '/changer?name'
+	else
+		value = "No"
+		redirect '/move_on'
+	end
+end
+
+get '/changer' do
+	name = params[:user_name]
+	erb :get_change, :locals => {:name => name}
+end
+
+# get '/' do
+# 	name = params[:user_name]
+# 	erb :get_change, :locals => {:name => name}
+# end
